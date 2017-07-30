a44ab9bed7dfea69414f6eb541b418c91eaa7213
bandworthy <s3240684@student.rmit.edu.au>
2017-07-30 21:03:45 +1000

    First Commit

---
+++ DeathPoint/src/net/fantastisk/deathpoint/DeathPoint.java
@@ -0,0 +1,101 @@
+package net.fantastisk.deathpoint;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Location;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDeathEvent;
+//import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class DeathPoint extends JavaPlugin implements Listener
+{
+	
+	//detects player has died and sends message
+	@EventHandler(priority=EventPriority.NORMAL)
+	public void OnDeath(EntityDeathEvent e)
+	{ 
+		
+		if(e.getEntity() instanceof Player)
+		{
+			Player player = (Player) e.getEntity();
+	        Location playerspot = player.getLocation();
+	        
+	        
+	       String[] playerLocation =  formatdouble(playerspot.getX(),playerspot.getY(),playerspot.getZ());
+	       player.sendMessage(ChatColor.RED+" "+ player.getName()+" Your death location was "+playerLocation[0]+" "+playerLocation[1]+" "+playerLocation[2]);
+	       //.sendMessage(player.getName()+" Your death location was "+playerLocation[0]+" "+playerLocation[1]+" "+playerLocation[2]);)
+	       sendtoConsole(player.getName()+" died! location was "+playerLocation[0]+" "+playerLocation[1]+" "+playerLocation[2]); 
+			//player.sendMessage("You died plugin test"+ playerspot.getX()+" "+playerspot.getY()+" "+playerspot.getZ());
+		}
+
+	}
+
+	public boolean onCommand(CommandSender sender, Command command, String label,String[] arguments)
+	{
+		//how to add commands
+		
+		command.getName();
+		
+		if(command.getName().equalsIgnoreCase("feedme"))
+		{
+			
+			if ( sender instanceof Player)
+			{	
+				Player player = (Player) sender;
+				player.setFoodLevel(20);
+				player.sendMessage(ChatColor.GREEN + "You have been feed!");
+			}
+			else
+			{
+				sender.sendMessage(ChatColor.RED + "You must be a player to be fed!");
+
+			}
+			return true;
+		}
+		
+		return false;
+		
+	}
+	
+    @Override
+    public void onEnable() 
+    {
+    	
+    	//needed to register listeners for events
+    	getServer().getPluginManager().registerEvents(this, this);
+    	
+    	
+    	//logic performed when plugin enabled
+    	//getLogger().info("onEnable has been invoked!");
+    	//getServer().getLogger().info("FeelMe has been loaded successfully!");
+    }
+
+    @Override
+    public void onDisable() 
+    {
+    	
+   	
+    	// logic performed when plugin disabled 
+    	//getLogger().info("onDisable has been invoked!");
+    	//getServer().getLogger().info("FeelMe has been unloaded successfully!");
+    }
+    
+    //takes 3 doubles normally coordinate values and shortens to 2 decimal places.
+    public String[] formatdouble(double valX,double valZ,double valY)
+    {
+    	String[] arrayformated = new String[] {String.format("%.2f", valX),String.format("%.2f", valZ),String.format("%.2f", valY)};
+    	return arrayformated;
+    }
+    //send message to server console
+    public void sendtoConsole(String message)
+    {
+    	Bukkit.getConsoleSender().sendMessage(message);
+    }
+    
+    
+}
