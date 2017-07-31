c7ed9c352f4b4acf66c9ebc8bee31f565f79b289
bandworthy <s3240684@student.rmit.edu.au>
2017-07-31 20:15:08 +1000

    adding DeathObject

---
+++ DeathPoint/src/net/fantastisk/deathpoint/DeathObject.java
@@ -0,0 +1,18 @@
+package net.fantastisk.deathpoint;
+
+public class DeathObject 
+{
+
+	public DeathObject (String name,String X,String Z, String Y)
+	{
+			String Name = name;
+			String PosX = X;
+			String PosZ = Z;
+			String PosY = Y;
+	}
+	
+	
+	
+
+
+}
